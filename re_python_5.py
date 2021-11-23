b606aed0564a7adc58a5059a3c6f0ff31c5feaad
Yoon-D-G <e_d_garrod@hotmail.com>
2021-11-23 15:46:12 +0000

    Polonius' advice to Laertes ... second_branch commit

--- re_python_5.py
+++ re_python_5.py
@@ -1,15 +1,27 @@
 import re
 
-test_string = """My heart aches, and a drowsy numbness pains
-         My sense, as though of hemlock I had drunk,
-Or emptied some dull opiate to the drains
-         One minute past, and Lethe-wards had sunk:
-'Tis not through envy of thy happy lot,
-         But being too happy in thine happiness,—
-                That thou, light-winged Dryad of the trees
-                        In some melodious plot
-         Of beechen green, and shadows numberless,
-                Singest of summer in full-throated ease. """
+test_string = """Give thy thoughts no tongue, 
+Nor any unproportioned thought his act. 60 
+Be thou familiar, but by no means vulgar. 
+Those friends thou hast, and their adoption tried, 
+Grapple them to thy soul with hoops of steel; 
+But do not dull thy palm with entertainment 
+Of each new-hatch’d, unfledged comrade. Beware 65 
+Of entrance to a quarrel, but being in, 
+Bear’t that the opposed may beware of thee. 
+Give every man thy ear, but few thy voice; 
+Take each man’s censure, but reserve thy judgment. 
+Costly thy habit as thy purse can buy, 70 
+But not express’d in fancy; rich, not gaudy; 
+For the apparel oft proclaims the man, 
+And they in France of the best rank and station 
+Are of a most select and generous chief in that. 
+Neither a borrower nor a lender be; 75 
+For loan oft loses both itself and friend, 
+And borrowing dulls the edge of husbandry. 
+This above all: to thine ownself be true, 
+And it must follow, as the night the day, 
+Thou canst not then be false to any man.. """
 
 pattern = re.compile(r'_?\d')
 matches = pattern.finditer(test_string)
